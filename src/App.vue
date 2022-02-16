8455599d7141cb368acd077f1692e271a7344357
twf <824437574@qq.com>
2022-02-16 15:06:02 +0800

    refactor(router): 修改了router的指向

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <Index></Index>
   </div>
 </template>
 
 <script>
+import Index from "./views/Index.vue";
 export default {
-  name: "App"
+  name: "App",
+  components:{
+    Index,
+  }
 };
 </script>
 
