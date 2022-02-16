7be11c1ff65e8e58824a95cf11e7b19b208225b9
twf <824437574@qq.com>
2022-02-16 11:47:16 +0800

    fix(all):恢复文件

---
+++ src/components/BackTop.vue
@@ -0,0 +1,63 @@
+<template>
+  <el-button icon="el-icon-arrow-up" circle @click="backtop" class="backtop" v-show="isShow"></el-button>
+</template>
+
+<script>
+export default {
+  name: "BackTop",
+  data() {
+    return {
+      isShow: true
+    }
+
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll, true)
+  },
+  methods: {
+    backtop() {
+      window.scrollTo(0, 0);
+    },
+    handleScroll() {
+      let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+      console.log(scrollTop);
+      if (scrollTop >= 60) {
+        this.isShow = true
+      } else {
+        this.isShow = false
+      }
+    }
+  },
+};
+</script>
+
+<style scoped>
+.backtop {
+  position: fixed;
+  right: 60px;
+  bottom: 60px;
+  z-index: 1000;
+}
+/* .backtop {
+  height: 60px;
+  width: 60px;
+  background-color: red;
+  border-radius: 50%;
+  box-shadow: 2px 2px 2px #cef;
+  position: relative;
+} */
+/* .backtop::before {
+  content: "";
+  border: 16px solid transparent;
+  position: absolute;
+  border-bottom-color: orange; */
+/* top: 50%; */
+/* left: 50%; */
+/* transform: translate(-50%);
+  z-index: 1000; */
+/* } */
+/* .backtop:hover {
+  background-color: #cef;
+  box-shadow: 2px 2px 2px red;
+} */
+</style>
